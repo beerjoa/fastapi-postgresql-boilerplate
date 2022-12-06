73409aaba60767b38cace5cbfac787fc98ea705b
Beerjoa <yoooong2@gmail.com>
2022-12-06 23:12:35 +0900

    fix: load db session
    - fix load db session logic
    - db dependency

---
+++ app/database/events.py
@@ -0,0 +1,29 @@
+import logging
+
+from fastapi import FastAPI
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+
+from app.core.settings.app import AppSettings
+
+logger = logging.getLogger(__name__)
+
+
+async def connect_to_db(app: FastAPI, settings: AppSettings) -> None:
+    logger.info("Connecting to database...")
+
+    engine = create_async_engine(
+        url=str(settings.db_url_conf.docker_db), pool_size=50, max_overflow=0, echo=True, future=True
+    )
+    async_session_factory = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False, autoflush=True)
+    app.state.pool = async_session_factory
+
+    logger.info("Connected to database.")
+
+
+async def close_db_connection(app: FastAPI) -> None:
+    logger.info("Closing database connection...")
+
+    app.state.pool.close_all()
+
+    logger.info("Database connection closed.")
